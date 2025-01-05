af3df682aa3aa98296dd923da46dfb34e49a412e
Pedro <Peanfloca@gmail.com>
2025-01-05 18:35:59 +0100

    1.0

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,12 +13,25 @@ public class App {
         Transaction transaction = null;
 
         try {
+
+
+
             transaction = session.beginTransaction();
             
             // Transient
             Casa casa = new Casa("Aluminio", "Madera", "100m²");
             Residencial residencial = new Residencial("Residencial 1", 100, "Distrito 1");
+/*
+            //Esto para modificar un dato de la casa con id 1
+            casa = session.get(Casa.class, 1);
+            casa.setVentana("Cristal");
 
+            //Esto para eliminar una casa con id 1
+            casa = session.get(Casa.class, 1);
+            if (casa != null) {
+                session.delete(casa);
+            }
+*/
             // Persistent
             session.persist(casa);  // Ahora 'casa' está en estado persistente
             session.persist(residencial);
